d45d7d531c616aad8cb6540fd3df2dca40f31ffa
Abhishek Singh <abhishek.ec@global.org.in>
2022-05-01 19:08:49 +0530

    Update FacialLandmarkDetection.py

--- FacialLandmarkDetection.py
+++ FacialLandmarkDetection.py
@@ -20,7 +20,7 @@ while True:
     results = faceMesh.process(imgRGB)
     if results.multi_face_landmarks:
         for faceLms in results.multi_face_landmarks:
-            mpDraw.draw_landmarks(img, faceLms,mpFaceMesh.FACE_CONNECTIONS, drawSpec, drawSpec)
+            mpDraw.draw_landmarks(img, faceLms,mpFaceMesh.FACEMESH_TESSELATION, drawSpec, drawSpec)
 
             for id,lm in enumerate(faceLms.landmark):
                 ih, iw, ic = img.shape
